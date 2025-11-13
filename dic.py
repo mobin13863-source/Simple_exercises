4752662700973f2365ce67ce05fb22c944c76119
Mobin <234991811+mobin13863-source@users.noreply.github.com>
2025-11-13 17:39:10 +0330

    Create dic.py
    this is a dict a i try somethinhg

---
+++ dic.py
@@ -0,0 +1,8 @@
+price={"apple": 200, "orange": 100, "bananas": 12}
+print(price)
+print(price.get('orange',-1))
+print(price.get("apple", -2))
+
+name={"mobin": [1,"mobin",19], "sina": [20,18,12,14]}
+print(name)
+print(name["mobin"][1  ].upper())
